0c402879a64f3ef2429fd8b57e8440dad9b57235
deing <admin@15318.de>
2019-05-19 17:33:50 +0200

    Add more stuff

--- src/TildeyII.cr
+++ src/TildeyII.cr
@@ -5,11 +5,29 @@ Dotenv.load!
 client = Discord::Client.new(token: "Bot #{ENV["token"]}")
 
 bot_id = client.get_current_user.id
+bot_name = client.get_current_user.username
 
 issue_regex = /#([0-9]+)/
 
 client.on_message_create do |message|
+  next if message.author.bot
   next unless message.mentions.any? { |user| user.id == bot_id }
+  if message.content.downcase =~ Regex.new("<@!?#{bot_id}> help")
+    client.create_message(message.channel_id, <<-STR
+    **__TildesAutolinker__**
+    Use `@#{bot_name} help` to see this message.
+    `@#{bot_name} UTW` links to the Unofficial Tildes Wiki.
+    To get other links (see below), mention me anywhere in your message.
+    `#123` is turned into <https://gitlab.com/tildes/tildes/issues/123>.
+    Hacked together by `deing#7141`.
+    STR
+    )
+    next
+  end
+  if message.content.downcase =~ Regex.new("<@!?#{bot_id}> utw")
+    client.create_message(message.channel_id, "<https://unofficial-tildes-wiki.gitlab.io>")
+    next
+  end
   issues = message.content.scan issue_regex
   output = ""
   issues.each do |issue|
